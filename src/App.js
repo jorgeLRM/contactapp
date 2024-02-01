b3599b71fd479c8cd8811d97c831128163e3005b
jorgeLRM <jorge.lrm99@gmail.com>
2024-02-01 17:52:48 -0600

    Add HTML Modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
-import Header from './components/Header';
+import Header from "./components/Header";
 import { getContact, getContacts } from "./api/ContactService";
 import { useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import ContactList from "./components/ContactList";
 
 function App() {
+  const modalRef = useRef();
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
 
-  const getAllContacts = async(page = 0, size = 10) => {
+  const getAllContacts = async (page = 0, size = 10) => {
     try {
       setCurrentPage(page);
       const { data } = await getContacts(page, size);
       setData(data);
       console.log(data);
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  const toogleModal = (show) => {console.log('I was click'); }
+  const toggleModal = (show) => {
+    show ? modalRef.current.showModal() : modalRef.current.close();
+  };
 
   useEffect(() => {
     getAllContacts();
@@ -27,7 +32,75 @@ function App() {
 
   return (
     <>
-      <Header toogleModal={toogleModal} nbOfContacts={data.totalElements} />
+      <Header toggleModal={toggleModal} nbOfContacts={data.totalElements} />
+      <main className="main">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Navigate to={"/contacts"} />} />
+            <Route
+              path="/contacts"
+              element={
+                <ContactList
+                  data={data}
+                  currentPage={currentPage}
+                  getAllContacts={getAllContacts}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </main>
+
+      {/* Modal */}
+      <dialog ref={modalRef} className="modal" id="modal">
+        <div className="modal__header">
+          <h3>New Contact</h3>
+          <i onClick={() => toggleModal(false)} className="bi bi-x-lg"></i>
+        </div>
+        <div className="divider"></div>
+        <div className="modal__body">
+          <form>
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details">Name</span>
+                <input type="text" name="name" required />
+              </div>
+              <div className="input-box">
+                <span className="details">Email</span>
+                <input type="text" name="email" required />
+              </div>
+              <div className="input-box">
+                <span className="details">Title</span>
+                <input type="text" name="title" required />
+              </div>
+              <div className="input-box">
+                <span className="details">Phone Number</span>
+                <input type="text" name="phone" required />
+              </div>
+              <div className="input-box">
+                <span className="details">Address</span>
+                <input type="text" name="address" required />
+              </div>
+              <div className="input-box">
+                <span className="details">Account Status</span>
+                <input type="text" name="status" required />
+              </div>
+              <div className="file-input">
+                <span className="details">Profile Photo</span>
+                <input type="file" name="photo" required />
+              </div>
+            </div>
+            <div className="form_footer">
+              <button onClick={() => toggleModal(false)} type="button" className="btn btn-danger">
+                Cancel
+              </button>
+              <button type="submit" className="btn">
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+      </dialog>
     </>
   );
 }
